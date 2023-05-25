f8f08c870dc0e66a632eaef6a768faadeb99276c
Kozyavin V <kioci@yandex.ru>
2023-05-25 21:18:34 +0300

    ChangeTest

--- src/test/java/FilmManagerTest.java
+++ src/test/java/FilmManagerTest.java
@@ -3,6 +3,7 @@ import org.junit.jupiter.api.Test;
 
 public class FilmManagerTest {
 
+    //////////////////////////////////////////тестирование метода findAll с пустым конструктором////////////////////////////
     @Test
     public void FindAllTest() {
         FilmManager manager = new FilmManager();           //тест метода вывода всего списка фильмов в порядке добавления
@@ -23,7 +24,7 @@ public class FilmManagerTest {
 
     @Test
     public void FindOneCurrentTest() {
-        FilmManager manager = new FilmManager();           //тест метода вывода всего списка фильмов в порядке добавления
+        FilmManager manager = new FilmManager();       //тест метода вывода всего списка фильмов в порядке добавления, когда всего 1 фильм
 
         manager.addFilm("Бладшот");
 
@@ -34,8 +35,9 @@ public class FilmManagerTest {
         Assertions.assertArrayEquals(expected, actual);
     }
 
+    //////////////////////////////////////////тестирование метода findLast с пустым конструктором////////////////////////////
     @Test
-    public void test1() {                               //тест метода findLast с конструктором (пустым) и параметром кол-ва фильмов по умолчанию -5
+    public void MoreLimitNullConstructorTest() {     //тест метода findLast с конструктором (пустым). Кол-во фильмов > чем в конструкторе.
         FilmManager manager = new FilmManager();
 
         manager.addFilm("Бладшот");
@@ -44,16 +46,58 @@ public class FilmManagerTest {
         manager.addFilm("Джентльмены");
         manager.addFilm("Человек-неведимка");
         manager.addFilm("Тролли.Мировой тур");
-        manager.addFilm("Номер один");
+
 
         String[] expected = {"Человек-неведимка", "Джентльмены", "Отель Белград", "Вперёд", "Бладшот"};
         String[] actual = manager.findLast();
 
         Assertions.assertArrayEquals(expected, actual);
     }
-
     @Test
-    public void test2() {                               //тест метода findLast с конструктором и параметром limit=4
+    public void MinLimitNullConstructorTest() {       //тест метода findLast с конструктором (пустым). Кол-во фильмов < чем в конструкторе.
+        FilmManager manager = new FilmManager();
+
+        manager.addFilm("Бладшот");
+        manager.addFilm("Вперёд");
+        manager.addFilm("Отель Белград");
+        manager.addFilm("Джентльмены");
+
+
+        String[] expected = {"Джентльмены", "Отель Белград", "Вперёд", "Бладшот"};
+        String[] actual = manager.findLast();
+
+        Assertions.assertArrayEquals(expected, actual);
+    }
+    @Test
+    public void OnBoundaryLimitNullConstructorTest() {    //тест метода findLast с конструктором (пустым). Кол-во фильмов = кол-во фильмов в конструкторе
+        FilmManager manager = new FilmManager();
+
+        manager.addFilm("Бладшот");
+        manager.addFilm("Вперёд");
+        manager.addFilm("Отель Белград");
+        manager.addFilm("Джентльмены");
+        manager.addFilm("Человек-неведимка");
+
+
+        String[] expected = {"Человек-неведимка", "Джентльмены", "Отель Белград", "Вперёд", "Бладшот"};
+        String[] actual = manager.findLast();
+
+        Assertions.assertArrayEquals(expected, actual);
+    }
+    @Test
+    public void MinFilmsNullConstructorTest() {    //тест метода findLast с конструктором (пустым). Кол-во фильмов = 1
+        FilmManager manager = new FilmManager();
+
+        manager.addFilm("Бладшот");
+
+        String[] expected = {"Бладшот"};
+        String[] actual = manager.findLast();
+
+        Assertions.assertArrayEquals(expected, actual);
+    }
+//////////////////////////////////////////тестирование метода findLast с конструктором, где задаём лимит///////////////////////
+    @Test
+    public void MoreLimitFindLastTest() {          //тест метода findLast с конструктором и параметром limit=4, а фильмов 5
         FilmManager manager = new FilmManager(4);
 
         manager.addFilm("Бладшот");
@@ -61,8 +105,7 @@ public class FilmManagerTest {
         manager.addFilm("Отель Белград");
         manager.addFilm("Джентльмены");
         manager.addFilm("Человек-неведимка");
-        manager.addFilm("Тролли.Мировой тур");
-        manager.addFilm("Номер один");
+
 
         String[] expected = {"Джентльмены", "Отель Белград", "Вперёд", "Бладшот"};
         String[] actual = manager.findLast();
@@ -71,98 +114,57 @@ public class FilmManagerTest {
     }
 
     @Test
-    public void test3() {                               //тест метода findLast с конструктором и параметром limit=5 (Граничное значение)
-        FilmManager manager = new FilmManager(5);
+    public void MinLimitFindLastTest() {         //тест метода findLast с конструктором и параметром limit=4, а фильмов 3
+        FilmManager manager = new FilmManager(4);
 
         manager.addFilm("Бладшот");
         manager.addFilm("Вперёд");
         manager.addFilm("Отель Белград");
-        manager.addFilm("Джентльмены");
-        manager.addFilm("Человек-неведимка");
-        manager.addFilm("Тролли.Мировой тур");
-        manager.addFilm("Номер один");
 
-        String[] expected = {"Человек-неведимка", "Джентльмены", "Отель Белград", "Вперёд", "Бладшот"};
+
+        String[] expected = {"Отель Белград", "Вперёд", "Бладшот"};
         String[] actual = manager.findLast();
 
         Assertions.assertArrayEquals(expected, actual);
     }
 
     @Test
-    public void test4() {                               //тест метода findLast с конструктором и параметром limit=6 (Класс эксивалентности выше лимита по умолчанию)
-        FilmManager manager = new FilmManager(6);
+    public void OnBoundaryLimitFindLastTest() {       //тест метода findLast с конструктором и параметром limit=4, фильмов 4
+        FilmManager manager = new FilmManager(4);
 
         manager.addFilm("Бладшот");
         manager.addFilm("Вперёд");
         manager.addFilm("Отель Белград");
         manager.addFilm("Джентльмены");
-        manager.addFilm("Человек-неведимка");
-        manager.addFilm("Тролли.Мировой тур");
-        manager.addFilm("Номер один");
 
-        String[] expected = {"Тролли.Мировой тур", "Человек-неведимка", "Джентльмены", "Отель Белград", "Вперёд", "Бладшот"};
+
+        String[] expected = {"Джентльмены", "Отель Белград", "Вперёд", "Бладшот"};
         String[] actual = manager.findLast();
 
         Assertions.assertArrayEquals(expected, actual);
     }
 
     @Test
-    public void test5() {                               //тест метода findLast с конструктором и параметром limit=8. Лимит больше ,чем фильмов
-        FilmManager manager = new FilmManager(8);
-
-        manager.addFilm("Бладшот");
-        manager.addFilm("Вперёд");
-        manager.addFilm("Отель Белград");
-        manager.addFilm("Джентльмены");
-        manager.addFilm("Человек-неведимка");
-        manager.addFilm("Тролли.Мировой тур");
-        manager.addFilm("Номер один");
-
-        String[] expected = {"Номер один", "Тролли.Мировой тур", "Человек-неведимка", "Джентльмены", "Отель Белград", "Вперёд", "Бладшот"};
-        String[] actual = manager.findLast();
-
-        Assertions.assertArrayEquals(expected, actual);
-    }
-
-    @Test
-    public void test6() {                               //тест метода findLast с конструктором и параметром limit=1.
+    public void MinFilmsFindLastTest() {         //тест метода findLast с конструктором и параметром limit=1, фильмов 1
         FilmManager manager = new FilmManager(1);
 
         manager.addFilm("Бладшот");
-
 
         String[] expected = {"Бладшот"};
         String[] actual = manager.findLast();
 
         Assertions.assertArrayEquals(expected, actual);
     }
+    @Test
+    public void NullLimitFindLastTest() {         //тест метода findLast с конструктором и параметром limit=0, фильмов 1
+        FilmManager manager = new FilmManager(0);
 
+        manager.addFilm("Бладшот");
+
+        String[] expected = new String[0];
+        String[] actual = manager.findLast();
+
+        Assertions.assertArrayEquals(expected, actual);
+    }
 }
 
-/*public class FilmBillTest {
-
-    FilmBill unit1 = new FilmBill(3,"Бладшот", "боевик");
-    FilmBill unit2 = new FilmBill(3,"Вперёд", "мультфильм");
-    FilmBill unit3 = new FilmBill(3,"Отель Белград", "комедия");
-    FilmBill unit4 = new FilmBill(3,"Человек-неведимка", "ужасы");
-    FilmBill unit5 = new FilmBill(3,"Тролли.Мировой тур", "мультфильм");
-    FilmBill unit6 = new FilmBill(3,"Номер один", "комедия");
-
-    @Test
-    public void test(){
-        FilmRepository repo = new FilmRepository();
-        repo.saveFilm(unit1);
-        repo.saveFilm(unit2);
-        repo.saveFilm(unit3);
-        repo.saveFilm(unit4);
-        repo.saveFilm(unit5);
-        repo.saveFilm(unit6);
-
-        FilmBill[] expected = {unit1,unit2,unit3,unit4,unit5,unit6};
-        FilmBill[] actual = repo.findAll();
-
-        Assertions.assertArrayEquals(expected,actual);
-
-
-
-    }*/
